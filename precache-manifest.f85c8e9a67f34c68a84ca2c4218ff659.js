35d6f7ff3af0052cfe43422d074ec7f736bd1e18
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2019-04-23 15:38:08 -0300

    Updates

---
+++ precache-manifest.f85c8e9a67f34c68a84ca2c4218ff659.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6ced59f567d87628b0f",
+    "url": "/todo-board/static/css/main.4688ddf9.chunk.css"
+  },
+  {
+    "revision": "b6ced59f567d87628b0f",
+    "url": "/todo-board/static/js/main.99418c26.chunk.js"
+  },
+  {
+    "revision": "53cfdde23cb13f47047e",
+    "url": "/todo-board/static/js/runtime~main.d1823f73.js"
+  },
+  {
+    "revision": "b6ed07673400ca588744",
+    "url": "/todo-board/static/js/2.ac0b6886.chunk.js"
+  },
+  {
+    "revision": "26a7cfb3505483d58268d67b2d352db0",
+    "url": "/todo-board/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-board/precache-manifest.d98f8e0ea32ab6f2c95b6a5117bce666.js"
+  "/todo-board/precache-manifest.f85c8e9a67f34c68a84ca2c4218ff659.js"
 );
 
 workbox.clientsClaim();
