d2e6d9f7a1cf3eacb4be616281096e818c85a0e6
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2019-04-23 15:30:32 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-board-firebase/precache-manifest.34be97ba946443bcf9ca44965075f93b.js"
+  "/todo-board/precache-manifest.14cca4233c01b48d837c3297fceb68d4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todo-board-firebase/index.html", {
+workbox.routing.registerNavigationRoute("/todo-board/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
