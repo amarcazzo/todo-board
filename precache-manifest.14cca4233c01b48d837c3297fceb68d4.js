f01c7cce77634f1db52db76f41f868643035a08d
Ayrton Marcazzo <ayrtonmarcazzo@gmail.com>
2019-04-23 16:36:38 -0300

    Updates

---
+++ precache-manifest.14cca4233c01b48d837c3297fceb68d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0662980fc3f52bd03cf4",
+    "url": "/todo-board/static/css/main.4688ddf9.chunk.css"
+  },
+  {
+    "revision": "0662980fc3f52bd03cf4",
+    "url": "/todo-board/static/js/main.a2c70ab0.chunk.js"
+  },
+  {
+    "revision": "53cfdde23cb13f47047e",
+    "url": "/todo-board/static/js/runtime~main.d1823f73.js"
+  },
+  {
+    "revision": "f8ebc4acf38f026ce8d4",
+    "url": "/todo-board/static/js/2.b97e481e.chunk.js"
+  },
+  {
+    "revision": "f399174c26f0cb8301c5c148acec05a8",
+    "url": "/todo-board/index.html"
+  }
+];
